85f880767c080e7b961b93a610f06ed5fb441026
Ajin <ajinvijayan0204@gmail.com>
2024-07-07 22:19:58 +0530

    Initial project created

---
+++ MovieBase/MovieBase/MovieBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBaseApp.swift
+//  MovieBase
+//
+//  Created by Ajin on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
