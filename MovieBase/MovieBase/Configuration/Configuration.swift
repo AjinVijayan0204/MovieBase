03a3521e5e489041c38572111b5cc308c2857e51
Ajin <ajinvijayan0204@gmail.com>
2024-07-10 23:34:45 +0530

    Added configurations

---
+++ MovieBase/MovieBase/Configuration/Configuration.swift
@@ -0,0 +1,28 @@
+//
+//  Configuration.swift
+//  MovieBase
+//
+//  Created by Ajin on 10/07/24.
+//
+
+import Foundation
+
+struct Strings{
+    static let configPlist = "App-Config"
+    static let plist = "plist"
+}
+
+struct Config{
+    var appUrl: String = ""
+    var apiKey: String = ""
+    
+    init(){
+        if let configFile = Bundle.main.path(forResource: Strings.configPlist, ofType: Strings.plist){
+            let fileUrl = URL(filePath: configFile)
+            if let details = try? NSDictionary(contentsOf: fileUrl, error: ()) as? [String: Any]{
+                self.appUrl = details["App_Base_Url"] as? String ?? ""
+                self.apiKey = details["api_key"] as? String ?? ""
+            }
+        }
+    }
+}
