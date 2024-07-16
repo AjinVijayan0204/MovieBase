9927784be941ffbf4e0ab15089d718306b51394c
Ajin <ajinvijayan0204@gmail.com>
2024-07-16 22:41:10 +0530

    Modified movie list view

--- MovieBase/MovieBase/Presentation/View/MovieListView.swift
+++ MovieBase/MovieBase/Presentation/View/MovieListView.swift
@@ -12,30 +12,53 @@ struct MovieListView: View {
     @StateObject var vm = MovieListViewModel()
     
     var body: some View {
-        ScrollView(.horizontal){
-            VStack {
-                HStack {
-                    ForEach(vm.movies, id: \.self){ movie in
-                        CardView(name: movie.originalTitle, url: Config().imgBaseUrl+movie.posterPath)
-                    }
+        ScrollView(.vertical) {
+            VStack(alignment: .leading){
+                AsyncImage(url: URL(string: "")) { img in
+                    img
+                        .resizable()
+                        .frame(height: Screen.shared.height * 0.30)
+                } placeholder: {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .frame(height: Screen.shared.height * 0.30)
+                        .tint(.white)
+                        .foregroundStyle(Color.white)
                 }
+                .padding(.top, Screen.shared.height * 0.1)
+
+                Text("Popular Movies")
+                    .sectionHeader()
+                HorizontalScrollView(listItems: $vm.popularMovies)
+                    .onAppear{
+                        vm.getPopularMovies()
+                    }
+                
+                Text("Top Rated")
+                    .sectionHeader()
+                HorizontalScrollView(listItems: $vm.topRated)
+                    .onAppear{
+                        vm.getTopRatedMovie()
+                    }
             }
-        }
-        .frame(width: Screen.shared.width, height: Screen.shared.height)
-        .background(content: {
-            Color.black
-        })
-        .onAppear{
-            vm.getMovies()
+            .frame(width: Screen.shared.width,height: .infinity, alignment: .top)
+            .padding(.leading)
         }
     }
 }
 
+
 struct HorizontalScrollView: View{
     
+    @Binding var listItems: [MovieCardModel]
+    
     var body: some View{
-        HStack{
-            
+        ScrollView(.horizontal) {
+            HStack{
+                ForEach(listItems, id: \.self){ movie in
+                    CardView(name: movie.originalTitle, url: Config().imgBaseUrl+movie.posterPath)
+                }
+            }
         }
     }
 }
