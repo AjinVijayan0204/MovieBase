19f8ff508882092a8b01fa4e7c47470ced69bc49
Ajin <ajinvijayan0204@gmail.com>
2024-07-16 22:44:57 +0530

    Added custom extensions Added detail movie view

---
+++ MovieBase/MovieBase/Presentation/Extensions/ViewExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  ViewExtensions.swift
+//  MovieBase
+//
+//  Created by Ajin on 16/07/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension View{
+    func sectionHeader()-> some View{
+        modifier(SectionHeader())
+    }
+}
---
+++ MovieBase/MovieBase/Presentation/ViewModels/MovieDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MovieDetailViewModel.swift
+//  MovieBase
+//
+//  Created by Ajin on 16/07/24.
+//
+
+import Foundation
+
+class MovieDetailViewModel: ObservableObject{
+    
+}
