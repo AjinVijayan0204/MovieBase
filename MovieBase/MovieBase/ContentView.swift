742b8b6ad3cb38ef923b68ba0e1ffae730e4d997
Ajin <ajinvijayan0204@gmail.com>
2024-07-17 23:13:40 +0530

    Added dependencies in content view

--- MovieBase/MovieBase/ContentView.swift
+++ MovieBase/MovieBase/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let dataSource: MovieAPIImpl
+    let movieRepository: MovieRepository
+    let movieUseCase: MovieUseCases
+    let movieListViewModel: MovieListViewModel
+    let movieDetailViewModel: MovieDetailViewModel
+    
+    init() {
+          UIScrollView.appearance().bounces = false
+        self.dataSource = MovieAPIImpl()
+        self.movieRepository = MovieReposImpl(dataSource: dataSource)
+        self.movieUseCase = MovieUseCases(repo: movieRepository)
+        self.movieListViewModel = MovieListViewModel(movieUseCase: movieUseCase)
+        self.movieDetailViewModel = MovieDetailViewModel(movieUseCase: movieUseCase)
+       }
+    
     var body: some View {
-        MovieListView()
+//        MovieListView(vm: movieListViewModel)
+//            .background(content: {
+//                Color.black
+//            })
+        MovieDetailView(movieDetailViewModel: movieDetailViewModel)
             .ignoresSafeArea()
     }
 }
