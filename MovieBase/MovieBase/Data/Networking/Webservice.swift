8a06da108bf021171f78372f85ce04bd62b1ef7c
Ajin <ajinvijayan0204@gmail.com>
2024-07-21 17:55:27 +0530

    Changed from auth token to api key

--- MovieBase/MovieBase/Data/Networking/Webservice.swift
+++ MovieBase/MovieBase/Data/Networking/Webservice.swift
@@ -13,18 +13,18 @@ class WebService{
     
     func makeRequest(for url: String, queryItems: [URLQueryItem]) async throws-> Data{
         let baseUrl = appConfig.appUrl
-        guard let fullUrl = URL(string: baseUrl + url) else { return Data() }
+        let authorisation = "?api_key=\(appConfig.apiKey)"
+        
+        guard let fullUrl = URL(string: baseUrl + url + authorisation) else { return Data() }
         var components = URLComponents(url: fullUrl, resolvingAgainstBaseURL: true)!
         let queryItems: [URLQueryItem] = queryItems
         components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
         
-        let authorisation = "Bearer " + appConfig.apiKey
         var request = URLRequest(url: components.url!)
         request.httpMethod = RequestMethod.get.rawValue
         request.timeoutInterval = 10
         request.allHTTPHeaderFields = [
-            "accept": "application/json",
-            "Authorization": authorisation
+            "accept": "application/json"
           ]
         
         let (data, _) = try await URLSession.shared.data(for: request)
