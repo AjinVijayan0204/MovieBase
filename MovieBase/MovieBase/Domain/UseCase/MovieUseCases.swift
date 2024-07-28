8646553b17e1d5eb155c5f887c1d66edfb4a2bfd
Ajin <ajin.vijayan@trenser.com>
2024-07-28 22:26:23 +0530

    Added protocol for use cases

--- MovieBase/MovieBase/Domain/UseCase/MovieUseCases.swift
+++ MovieBase/MovieBase/Domain/UseCase/MovieUseCases.swift
@@ -7,15 +7,8 @@
 
 import Foundation
 
-struct MovieUseCases: MovieRepository{
-    
-    var repo: MovieRepository
-    
-    func getMovies(_ ofType: MovieEndpoint, page: Int) async -> [MovieCardModel] {
-        return (await repo.getMovies(ofType, page: page))
-    }
-    
-    func getDetail(_ ofType: MovieEndpoint) async -> MovieDetailModel? {
-        return (await repo.getDetail(ofType))
-    }
+protocol  MovieUseCases{
+    func getMovies(_ ofType: MovieEndpoint, page: Int) async -> [MovieCardModel]
+    func getDetail(_ ofType: MovieEndpoint) async -> MovieDetailModel?
 }
+
