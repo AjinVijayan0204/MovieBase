388d71ae853e9ef802a11325977bc00913546151
Ajin <ajinvijayan0204@gmail.com>
2024-07-30 20:32:25 +0530

    Added core data model

---
+++ MovieBase/MovieBase/Data/Swift Data/Swift DataModels/SwiftDataModels.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftDataModels.swift
+//  MovieBase
+//
+//  Created by Ajin on 30/07/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+struct MovieDataModel{
+    let movieId: Int
+    let adult: Bool
+    let originalLanguage: String
+    let originalTitle: String
+    let overview: String
+    let releaseDate: String
+    let title: String
+    let runtime: Double
+    let posterPath: String
+    let voteAvg: Double
+    
+    init(movieId: Int, adult: Bool, originalLanguage: String, originalTitle: String, overview: String, releaseDate: String, title: String, runtime: Double, posterPath: String, voteAvg: Double) {
+        self.movieId = movieId
+        self.adult = adult
+        self.originalLanguage = originalLanguage
+        self.originalTitle = originalTitle
+        self.overview = overview
+        self.releaseDate = releaseDate
+        self.title = title
+        self.runtime = runtime
+        self.posterPath = posterPath
+        self.voteAvg = voteAvg
+    }
+}
